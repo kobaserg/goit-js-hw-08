cbdc769b373524d9cea6b8b21cd1942bc1254d0e
kobaserg <kobasts@gmail.com>
2022-08-27 19:16:46 +0200

    ДЗ8 Задание 2 без LODASH

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('play', onPlay);
+
+function onPlay() {
+  let timeUpdate = localStorage.getItem('videoplayer-current-time');
+  player.setCurrentTime(timeUpdate);
+
+  player.on('timeupdate', onUpdateTime);
+
+  function onUpdateTime(seconds) {
+    localStorage.setItem('videoplayer-current-time', seconds.seconds);
+  }
+}
