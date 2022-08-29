cea57527e7f0b3cc367105c21fa4c6ce313cdec6
kobaserg <kobasts@gmail.com>
2022-08-29 11:58:56 +0200

    ДЗ8 Задание 3 доработка

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+const inputEmail = document.querySelector('input');
+const inputMessage = document.querySelector('textarea');
+const inputSabmit = document.querySelector('button');
 
+let storageForm = { email: '', message: '' };
+
+let localStorageObject = localStorage.getItem(
+  'feedback-form-state',
+  JSON.stringify(storageForm)
+);
+let insertStorageForm = JSON.parse(localStorageObject);
+
+if (insertStorageForm === null) {
+  insertStorageForm = { email: '', message: '' };
+}
+
+inputSabmit.addEventListener('click', onSubmitForm);
+inputEmail.addEventListener('input', onInputEmail);
+inputMessage.addEventListener('input', onInputMessage);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  console.log(
+    `SUBMIT --> email : ${inputEmail.value}  message : ${inputMessage.value}`
+  );
+  localStorage.removeItem('feedback-form-state');
+  inputEmail.value = '';
+  inputMessage.value = '';
+}
+inputEmail.value = insertStorageForm.email;
+inputMessage.value = insertStorageForm.message;
+
+function onInputEmail(event) {
+  storageForm.email = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(storageForm));
+}
+
+function onInputMessage(event) {
+  storageForm.message = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(storageForm));
+}
